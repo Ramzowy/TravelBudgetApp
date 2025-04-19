4fe6fa6a56c9b71d4011b3f044060e54afb171cc
Rahim <zowangrm@mail.uc.edu>
2025-04-19 14:14:36 -0400

    Created the MainPage

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -1,3 +1,5 @@
+using TravelBudgetApp.Services;
+
 namespace TravelBudgetApp.Views;
 
 public partial class MainPage : ContentPage
@@ -5,5 +7,6 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+        BindingContext = new ViewModels.MainPageViewModel(new ExchangeRateService());
+    }
 }
